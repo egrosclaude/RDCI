4622008778ae4aa3f97736a3f2683fd0fd1f6a37
egrosclaude <eduardo.grosclaude@gmail.com>
2016-10-11 13:40:04 -0300

    coursemod

--- externals/coursemod/coursemod.js
+++ externals/coursemod/coursemod.js
@@ -1,48 +1,63 @@
 (function () {
     'use strict';
+
     var config = Reveal.getConfig();
 
     if (config === null || typeof config.coursemod === 'undefined' || !config.coursemod.enabled)
         return;
+    
+    //disable when print-pdf is enabled
+    if (window.location.href.indexOf('print-pdf') > -1)
+        return;
+
 
     var holders = {
         presentation: undefined,
-        courseView: undefined
+        courseView: undefined,
+        coursemodIsShown: false
     };
 
+    function loadStylesheet() {
+        var path = undefined;
+        [].slice.call(document.getElementsByTagName('script')).forEach(function(script){
+            if(script.src.indexOf('coursemod.js') > - 1) {
+                path = script.src.split('/').slice(0, -1).join('/')+'/';
+            }
+        });
+
+        var link = window.document.createElement('link');
+        link.rel = 'stylesheet';
+        link.href = path + 'coursemod.css';
+        window.document.getElementsByTagName('head')[0].appendChild(link);
+    }
+
     function setup() {
+        loadStylesheet();
         holders.presentation = document.querySelector('.reveal');
-        holders.presentation.style.width = '100%';
-        holders.presentation.style.position = 'relative';
-        holders.presentation.style.display = 'inline-block';
-        var controls = document.querySelector('.controls');
-        controls.style.position = 'absolute';
+        holders.presentation.classList.add('coursemod__presentation');
 
         var courseView = document.createElement('div');
-        courseView.className = 'coursemod__course-view';
-        courseView.style.width = '33%';
-        courseView.style.height = '100%';
-        courseView.style.backgroundColor = 'white';
-        courseView.style.boxSizing = 'border-box';
-        courseView.style.borderLeft = '2px solid gray';
-        courseView.style.display = 'none';
-        courseView.style.overflowX = 'scroll';
-        courseView.style.padding = '0 20px';
-        courseView.style.position = 'fixed';
-        courseView.style.top = '0';
-        courseView.style.right = '0';
+        courseView.classList.add('coursemod__course-view');
         holders.presentation.parentNode.insertBefore(courseView, holders.presentation.nextElementSibling);
         holders.courseView = courseView;
     }
 
-    function toggleCourseView() {
-        if(config.coursemod.shown){
-            holders.presentation.style.width = '66.66%';
-            holders.courseView.style.display='inline-block';
-        }else {
-            holders.courseView.style.display='none';
-            holders.presentation.style.width = '100%';
+    function getSlideOverride(slide){
+        if (typeof slide.dataset.coursemodShown !== 'undefined'){
+            return slide.dataset.coursemodShown === 'true';
         }
+        return undefined;
+    }
+
+    function toggleCourseView(show) {
+        if(!holders.coursemodIsShown && show){
+            holders.presentation.classList.add('coursemod--active')
+            holders.courseView.classList.add('coursemod--active')
+        }else if(holders.coursemodIsShown){
+            holders.presentation.classList.remove('coursemod--active')
+            holders.courseView.classList.remove('coursemod--active')
+        }
+        holders.coursemodIsShown = show;
     }
 
     function updateNotes(currentSlide) {
@@ -63,17 +78,29 @@
     setup();
     updateNotes(Reveal.getCurrentSlide());
 
-    toggleCourseView();
+    var slideOverride = getSlideOverride(Reveal.getCurrentSlide());
+    if(typeof slideOverride === 'undefined'){
+        toggleCourseView(config.coursemod.shown);
+    }else{
+        toggleCourseView(slideOverride);
+    }
 
     Reveal.configure({
         keyboard: {
             67: function() {
                 config.coursemod.shown = !config.coursemod.shown;
-                toggleCourseView()
+                toggleCourseView(config.coursemod.shown)
             }
         }
     });
     Reveal.addEventListener( 'slidechanged', function( event ) {
-        updateNotes(event.currentSlide);
+        var currentSlide = event.currentSlide;
+        if (typeof currentSlide.dataset.coursemodShown !== 'undefined'){
+            var show = currentSlide.dataset.coursemodShown === 'true';
+            toggleCourseView(show);
+        }else {
+            toggleCourseView(config.coursemod.shown);
+        }
+        updateNotes(currentSlide);
     } );
 }).call(this);
