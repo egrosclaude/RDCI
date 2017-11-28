cb0bfc55fd601023d9b1346f9797628705dcdd15
Eduardo Grosclaude <eduardo.grosclaude@gmail.com>
2017-11-28 09:10:02 -0300

    mm

---
+++ externals/kenburns/kenburns.js
@@ -0,0 +1,53 @@
+var j = 0;
+var kenburns = function(id,images,interv) {
+
+	function setStyle() {
+		var style = document.createElement('style');
+		//style.type = 'text/css';
+		style.id = 'kenBurnsStyle';
+		console.log(style);
+		document.getElementsByTagName('head')[0].appendChild(style);
+	}
+	function kbStyle(id, url, x, y, zoom) {
+		var styleString = `
+			#${id} {
+				width: 100%;
+				height: 100%;
+				background-size: cover;
+				overflow: hidden;
+				animation: kenburns 15s infinite;
+				background-image: url(${url});
+			}
+			@keyframes kenburns {
+				0% {
+					opacity: 0;
+				}
+				5% {
+					opacity: 1;
+				}
+				95% {
+					transform: scale3d(${zoom},${zoom},${zoom}) translate3d(${x}px,${y}px, 0px);
+					animation-timing-function: ease-in;
+					opacity: 1;
+				}
+				100% {
+					transform: scale3d(2, 2, 2) translate3d(-${x}px, -${y}px, 0px);
+					opacity: 0;
+				}
+			}`;
+		console.log(styleString);
+		return styleString;
+	}
+
+	if(j++===0) setStyle();
+
+	var img = images[j % images.length];
+	var p = Math.random() * 100 - 50;
+	var q = Math.random() * 30 - 15;
+	var z = Math.random() + 1;
+			
+	document.getElementById('kenBurnsStyle').innerHTML = kbStyle(id, img, p, q, z);
+	console.log(document.getElementById('kenBurnsStyle').innerHTML);
+	setTimeout(function() { kenburns(id, images, interv) }, interv);
+};
+
